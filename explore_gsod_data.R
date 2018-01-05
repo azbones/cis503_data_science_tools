a41c0e715cd96f7c8d585c1825ddbee630e1aff8
Sellman, Collin <collin.sellman@pearson.com>
2018-01-04 17:46:25 -0700

    new files

---
+++ explore_gsod_data.R
@@ -0,0 +1,16 @@
+# exploring the gsod data for Arizona
+
+# This is the query to pull back the data from Google's BigQuery
+#
+# SELECT b.name, b.country, b.state, b.call, b.lat, b.lon, b.elev, b.begin, b.end, a.wban, 
+#        a.stn, a.year, a.mo, a.da, a.temp, a.count_temp, a.dewp, a.count_dewp, a.stp, a.count_stp, 
+#        a.visib, a.count_visib, a.wdsp, a.count_wdsp, a.gust, a.max, a.flag_max, a.min, a.flag_min, 
+#        a.prcp, a.flag_prcp, a.sndp, a.fog, a.rain_drizzle, a.snow_ice_pellets, a.hail, a.thunder, 
+#        a.tornado_funnel_cloud
+# FROM `bigquery-public-data.noaa_gsod.gsod*` a
+# JOIN `bigquery-public-data.noaa_gsod.stations` b ON a.stn=b.usaf AND a.wban=b.wban
+# WHERE state LIKE "AZ""""
+
+
+library(readr)
+azgsod <- read_csv("~/your path here/cis503_data_science_tools/data/azgsod.zip")
