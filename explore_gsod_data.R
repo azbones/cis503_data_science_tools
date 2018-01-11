3bdc314c65664aac6d4050b440712d17a3ce13ce
Sellman, Collin <collin.sellman@pearson.com>
2018-01-10 17:38:08 -0700

    adding more steps

--- explore_gsod_data.R
+++ explore_gsod_data.R
@@ -1,4 +1,13 @@
-# exploring the gsod data for Arizona
+# example data science workflow
+
+# GETTING THE DATA
+
+library(readr)
+library(tidyverse)
+
+setwd("~/Documents/My_Documents/ASU/cis 503/cis503_data_science_tools/")
+azgsod <- read_csv("./data/azgsod.zip")
+View(azgsod)
 
 # This is the query to pull back the data from Google's BigQuery (note it is using standard SQL dialect)
 #
@@ -12,12 +21,167 @@
 # WHERE state LIKE "AZ"
 
 
-library(readr)
-azgsod <- read_csv("./data/azgsod.zip")
+# CHECK THE INTEGRITY OF THE DATA
+# metadata and schema for data- https://www7.ncdc.noaa.gov/CDO/GSOD_DESC.txt
+# metadata and scheme for stations- ftp://ftp.ncdc.noaa.gov/pub/data/noaa/isd-history.txt 
+
+# did the import work correctly?
+lapply(azgsod, typeof)
+?lapply
+
+# does the data seem correct?
+lapply(azgsod, range)
+
+# CLEANING THE DATA
+# remove old school convention of setting missing data to nines
+# azgsod$dewp[azgsod$dewp == 9999.9] <- NA # boolean mask
+
+azgsod <- read_csv("./data/azgsod.zip", na = c('9999.9','999.9'))
+
+
+# EXPLORE THE DATA 
+
+# let's count the number of observations by station
+
+azgsod %>%
+  count(name) 
+
+# add print to add more lines to output
+
+# now let's arrange the output by the count in descending order using 
+arrange(desc())
+
+# look at observations by year
+
+azgsod %>%
+  count(year) %>%
+  print(n=100)
+
+# RESEARCH QUESTION- has the temperature changed over the duration of the dataset?
+
+# Let's start to visualize the data by looking at temp by year
+azgsod %>%
+    ggplot(aes(year, temp)) + geom_point()
+
+# What kind of data type is year?
+
+
+# Let's add some datetime types in.
+azgsod %>%
+mutate(yrmoda = ISOdatetime(.$year, .$mo, .$da, 0, 0, 0)) %>%
+  ggplot(aes(yrmoda, temp)) + geom_line()
+
+# Let's focus the time period to look at the pattern
+
+
+# Instead of looking at all the datapoints, let's group
+
+azgsod %>% 
+  group_by(year) %>% 
+  summarise(mean_temp =mean(temp)) %>% 
+  ggplot(aes(x=year, y=mean_temp)) + geom_point()
+
+# Let's look at the number of data points by station
+
+azgsod %>%
+  group_by(name) %>%
+  summarise(count_temp = n()) %>%
+  ggplot(aes(count_temp)) + geom_histogram()
+ 
+# focus
+azgsod %>% 
+  filter(name == 'DAVIS-MONTHAN AFB AIRPORT') %>%
+  count(year) %>%
+  print(n=100)
+
+azgsod %>% 
+  filter(name == 'DAVIS-MONTHAN AFB AIRPORT') -> davis
+
+davis %>%
+  select(year, mo, da, temp) %>%
+  mutate(yrmoda = ISOdatetime(.$year, .$mo, .$da, 0, 0, 0)) %>%
+  ggplot(aes(yrmoda, temp)) + geom_point()
+
+davis %>%
+  filter(year == 1980) %>%  # TODO: take out in order to add back in on the fly
+  select(year, mo, da, temp) %>%
+  mutate(yrmoda = ISOdatetime(.$year, .$mo, .$da, 0, 0, 0)) %>%
+  ggplot(aes(yrmoda, temp)) + geom_line()
 
 
 
-azgsod %>% group_by(mo) %>% summarise(count=n(), mean_temp =mean(temp))
+davis %>%
+  group_by(year, mo) %>%
+  summarise(mean= mean(temp)) -> davis_monthly
+
+library(modelr)
+
+davis_mod <- lm(mean ~ factor(mo), data= davis_monthly)
+
+summary(davis_mod)
+
+# plot model predictions
+
+davis_monthly %>% 
+  add_predictions(davis_mod) %>%
+  ggplot(aes(year + mo/12, pred)) + geom_line()
+
+# plot residuals
+
+davis_monthly %>% 
+  add_residuals(davis_mod) %>%
+  ggplot(aes(year + mo/12, resid)) + geom_point()
+
+
+davis %>%
+  group_by(year) %>%
+  summarise(mean_mo_temp = max(temp)) %>%
+  ggplot(aes(year, mean_mo_temp)) + geom_point() +stat_smooth()
+
+davis %>%
+  filter(mo == 7) %>%
+  group_by(year,mo) %>%
+  summarise(mean_mo_temp = max(temp)) %>%
+  mutate(yrmoda = ISOdatetime(.$year, .$mo, .$da, 0, 0, 0)) %>%
+  ggplot(aes(year, mean_mo_temp)) + geom_point() +stat_smooth()
+
+davis$year + davis$mo
+
+
+library(maps)
+
+# fun with mapping
+
+# create a base plot to draw on
+p <- ggplot() +
+  coord_fixed() +
+  xlab("") +
+  ylab("")
+
+# get the state data to draw state map
+us_map <- map_data("state")
+az_map <- subset(us_map, us_map$region=="arizona")
+
+# create the base az map 
+base_az_map <- p + geom_polygon(data=az_map, aes(x=long, y=lat, group=group), 
+                                     colour="light blue", fill="light blue")
+
+# filter for just the unique stations lat and longs
+azgsod %>%
+  select(name, lat, lon) %>%
+  unique() -> az_station_locs
+
+# add the station locations to the base plot and base Arizona map
+base_az_map +
+  geom_point(data=az_station_locs, aes(x=lon, y=lat), color="dark blue", size=3, alpha=.2)
+
+
+
+
+
+azgsod %>% 
+  group_by(mo) %>% 
+  summarise(count=n(), mean_temp =mean(temp))
 
 
 azgsod %>% group_by(year) %>% summarise(count=n(), mean_temp =mean(temp)) %>% ggplot(aes(x=year, y=mean_temp)) +geom_line()
